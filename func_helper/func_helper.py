363d528c0dd2749ea24c69635a586e3090db55e5
twined quartz <fmorisato@gmail.com>
2018-10-04 16:35:51 +0900

    add docstring

--- func_helper/func_helper.py
+++ func_helper/func_helper.py
@@ -2,15 +2,15 @@
 from functools import reduce
 
 
-def identity(df):
-    return df
+def identity(a):
+    return a
 
 
 def over_args(fn, transforms):
     """
     double = lambda x: x*2
     square = lambda x: x**2
-    over_args(lambda *arg: [*arg], [square, double])()
+    over_args(identity, [square, double])(1,5) == [2, 25]
     """
     def f(*args):
         return fn(
@@ -26,6 +26,15 @@ def over_args(fn, transforms):
 def compose(*funcs):
     """
     [f,g,h] -> x -> f(g(h(x)))
+
+    Usage
+    -----
+    composed = compose(
+        square,
+        add(5)
+    )
+
+    compose(0) is 25
     """
     def f(arg):
         return reduce(
@@ -39,6 +48,15 @@ def compose(*funcs):
 def pip(*funcs):
     """
     [f,g,h] -> x -> h(g(f(x)))
+
+    Usage
+    -----
+    piped = pip(
+        square,
+        add(5)
+    )
+
+    compose(0) is 5
     """
     def g(a):
         return reduce(
@@ -71,6 +89,22 @@ def Y(f):
     """
     F -> (a -> F.a) ->
     (x -> F.(a -> x.x.y)).(x -> F.(a -> x.x.y))
+
+    Usage
+    -----
+    def fib_template(f):
+
+        # F -> (a -> F.a)
+
+        def func(arg):
+            n = int(arg)
+            if n < 2:
+                return 1
+            else:
+                return f(n-1) + f(n-2)
+        return func
+
+    fib = Y(fib_template)
     """
     return (lambda x: f(lambda y: x(x)(y)))(lambda x: f(lambda y: x(x)(y)))
 
@@ -85,7 +119,6 @@ def recursiveExtender(decorator):
 
     Usage
     -----
-
     def fib_template(f):
 
         # F -> (a -> F.a)
@@ -105,6 +138,19 @@ def recursiveExtender(decorator):
 
 
 def tee(f):
+    """
+    Occur side effect without no effect on arguments.
+
+    Usage
+    -----
+    pip(
+        add(10),
+        pip(print),
+        add(10)
+    )(0) is 20
+
+    # output on stdout: 10
+    """
     def g(a):
         f(a)
         return a
