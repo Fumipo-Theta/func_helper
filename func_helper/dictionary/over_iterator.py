45ed4a20232045c9d6185be6f976bdde8c052790
twined quartz <fmorisato@gmail.com>
2019-01-09 19:24:00 +0900

    expand args

--- func_helper/dictionary/over_iterator.py
+++ func_helper/dictionary/over_iterator.py
@@ -1,2 +1,3 @@
-def over_iterator(d):
-    return lambda it: dict(zip(d.keys(), map(lambda f: [f(i) for i in it], d.values())))
+def over_iterator(d: dict={}, **kwargs):
+    _d = {**d, **kwargs}
+    return lambda it: dict(zip(_d.keys(), map(lambda f: [f(i) for i in it], _d.values())))
