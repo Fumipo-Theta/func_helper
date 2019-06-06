b67fb202e836e171c490d45584dd700b2c03a929
twined quartz <fmorisato@gmail.com>
2019-06-06 21:20:29 +0900

    band_factor

--- func_helper/dataframe/dataframe.py
+++ func_helper/dataframe/dataframe.py
@@ -1,10 +1,13 @@
 import pandas as pd
+import math
 import numpy as np
-from typing import List, Tuple, Iterator, TypeVar
+from typing import List, Tuple, Iterator, TypeVar, Union, Callable, Optional
 from functools import reduce
 
 T = TypeVar('T')
+Number = Union[int, float, complex]
 
+identity = lambda a: a
 
 def time_range(f):
     def wrap(*arg, **kwargs):
@@ -264,3 +267,135 @@ def create_complete_block_designed_df(mat_selector, group_selector, block_select
         ).dropna()
 
     return apply
+
+
+def create_bands(ini: float, fin: float, step: float, with_outer: bool=True)\
+        -> List[Tuple[Number, Number]]:
+    """
+    Create list of tuples defining lower and upper limits.
+
+    Parameters
+    ----------
+    ini: float
+        The first boundary of bands.
+    fin: float
+        The second boundary of bands.
+    step: float
+        Width of each band.
+    with_outer: bool
+        If true, bands of outside of [ini, fin] (or [fin, ini])
+            are added.
+        Therefore, returns [(-math.inf, ini), (ini, ini+step),...
+            (,ini+n*step), (ini+n*step,math.inf)]
+        Default is True.
+
+    Return
+    ------
+    level: list[tuple[float]]
+    """
+    n = int((fin-ini)/step)
+    outer = [(-math.inf, ini), (ini+n*step, math.inf)]\
+        if ini <= fin else\
+        [(math.inf, ini), (ini+n*step, -math.inf)]
+    if n < 0:
+        raise SystemError("Invalid sign of step.")
+
+    level = []
+    if with_outer:
+        level.append(outer[0])
+    for i in range(n):
+        level.append((ini + i*step, ini+(i+1)*step))
+    if with_outer:
+        level.append(outer[1])
+    return level
+
+
+def _get_factor_of_bands(bands: List[Tuple[Number, Number]])->List[str]:
+    """
+    """
+    def stringifier(band: Tuple[Number, Number]):
+        fst, snd = band
+        _fst = "" if fst == -math.inf else\
+            "" if fst == math.inf else\
+            str(fst)
+        _snd = "" if snd == -math.inf else\
+            ""if snd == math.inf else\
+            str(snd)
+        return _fst+"~"+_snd if fst <= snd else _snd+"~"+_fst
+    return list(map(stringifier, bands))
+
+def _get_mean_of_bands(bands: List[Tuple[Number,Number]])->List[Number]:
+    def apply(band: Tuple[Number,Number])->Number:
+        fst,snd = band
+        return 0.5*(fst+snd)
+    return list(map(apply,bands))
+
+def _get_lower_of_bands(bands):
+    return list(map(lambda band: band[0],bands))
+
+def _get_upper_of_bands(bands):
+    return list(map(lambda band: band[1],bands))
+
+def _get_index_of_bands(bands):
+    return list(map(lambda item: item[0],enumerate(bands)))
+
+def _i_position_selector(position_type):
+    if position_type is "index":
+        return _get_index_of_bands
+    if position_type is "center":
+        return _get_mean_of_bands
+    if position_type is "lower":
+        return _get_lower_of_bands
+    if position_type is "upper":
+        return _get_upper_of_bands
+    raise Exception(f"position_type{position_type} can not be recognized ! Choose 'index|lower|center|upper'.")
+
+def band_factor(
+    bands: List[Tuple[Number, Number]],
+    position: str="index"
+    )->Callable[[pd.DataFrame, Optional[str]], Tuple[pd.Series, List[str]]]:
+    """
+    Limit of bands must increase monotonic.
+
+    Domains are (lower, upper], or lower < x <= upper.
+
+    Usage
+    -----
+    cat_depth = categorize_by_bands(depth_bands)(df, "depth")
+    cat_depth = categorize_by_bands(depth_bands)(df, lambda df: df["depth"])
+    cat_depth = categorize_by_bands(depth_bands)(df, lambda df: df["depth"] * df["depth_factor"])
+
+    """
+    factor = _get_factor_of_bands(bands)
+    position = _i_position_selector(position)(bands)
+    cut_array = list(map(lambda band: band[0], bands))
+    cut_array.append(bands[-1][1])
+
+    def apply_to_df(df: pd.DataFrame, selector: Optional[str]=None)\
+            ->Tuple[pd.Series, List[str]]:
+
+        target = df if selector is None else df[selector]
+
+        return (pd.cut(target, cut_array, labels=factor), factor, position)
+    return apply_to_df
+
+
+
+
+assert(create_bands(-1, 10, 2, False) ==
+       [(-1, 1), (1, 3), (3, 5), (5, 7), (7, 9)])
+assert(create_bands(0, 10, 2, False) == [
+       (0, 2), (2, 4), (4, 6), (6, 8), (8, 10)])
+assert(create_bands(10, 0, -2, False) ==
+       [(10, 8), (8, 6), (6, 4), (4, 2), (2, 0)])
+assert(create_bands(10, 0, -2, True) ==
+       [(math.inf, 10), (10, 8), (8, 6), (6, 4), (4, 2), (2, 0), (0, -math.inf)])
+
+assert(list(_get_factor_of_bands(create_bands(-1, 10, 2, False)))
+       == ['-1~1', '1~3', '3~5', '5~7', '7~9'])
+assert(list(_get_factor_of_bands(create_bands(-1, 10, 2, True)))
+       == ['~-1', '-1~1', '1~3', '3~5', '5~7', '7~9', '9~'])
+assert(list(_get_factor_of_bands(create_bands(10, 0, -2, True)))
+       == ['10~', '8~10', '6~8', '4~6', '2~4', '0~2', '~0'])
+
+
